a8231846dca0df4c17a07196e65453c53afa62be
Arnav-Agarwal-01 <arnav22agarwal@gmail.com>
2024-12-09 12:24:51 +0530

    ........
    ............

--- Backend/index.js
+++ Backend/index.js
@@ -58,10 +58,10 @@ app.use('/blogGenie', blogRoute);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`
-// ⚡ Server is running at http://localhost:${PORT}/blogGenie
-// ✓ API endpoints ready
-// `);
-// });
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`
+⚡ Server is running at http://localhost:${PORT}/blogGenie
+✓ API endpoints ready
+`);
+});
+//module.exports = app;
